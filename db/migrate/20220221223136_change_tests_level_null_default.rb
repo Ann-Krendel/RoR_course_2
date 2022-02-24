95f44b5e7c95ce8954e7af4d70d0e2585b76e2a1
Anna Krendeleva <krendelevaanna@mail.ru>
2022-02-24 20:40:44 +0300

    Unit2_task2_v2

---
+++ db/migrate/20220221223136_change_tests_level_null_default.rb
@@ -0,0 +1,5 @@
+class ChangeTestsLevelNullDefault < ActiveRecord::Migration[7.0]
+  def change
+    change_column_default(:tests, :level, 0)
+  end
+end
---
+++ db/migrate/20220221223244_change_answers_correct__default.rb
@@ -0,0 +1,5 @@
+class ChangeAnswersCorrectDefault < ActiveRecord::Migration[7.0]
+  def change
+    change_column_default(:answers, :correct, false)
+  end
+end
