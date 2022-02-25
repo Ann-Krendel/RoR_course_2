87ae2402140f8b9530a17ad34a666c14e04301dd
Anna Krendeleva <krendelevaanna@mail.ru>
2022-02-25 19:00:42 +0300

    migrations

---
+++ db/migrate/20220225153848_add_answers_body_null_constraint.rb
@@ -0,0 +1,6 @@
+class AddAnswersBodyNullConstraint < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null(:answers, :body, false)
+    change_column_null(:answers, :correct, false)
+  end
+end
---
+++ db/migrate/20220225155758_add_answers_correct_default.rb
@@ -0,0 +1,5 @@
+class AddAnswersCorrectDefault < ActiveRecord::Migration[7.0]
+  def change
+    change_column_default(:answers, :correct, false)
+  end
+end
