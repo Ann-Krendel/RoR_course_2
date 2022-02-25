4832aad5ebdd321bc9e03a20f30c8df24d8f0c0e
Anna Krendeleva <krendelevaanna@mail.ru>
2022-02-25 13:40:12 +0300

    delete files

--- db/migrate/20220221200324_add_question_null_constraint.rb
+++
@@ -1,5 +0,0 @@
-class AddQuestionNullConstraint < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null(:questions, :body, false)
-  end
-end
--- db/migrate/20220221205017_add_answers_body_null_constraint.rb
+++
@@ -1,6 +0,0 @@
-class AddAnswersBodyNullConstraint < ActiveRecord::Migration[7.0]
-  def change
-    change_column_null(:answers, :body, false)
-    change_column_null(:answers, :correct, false)
-  end
-end
