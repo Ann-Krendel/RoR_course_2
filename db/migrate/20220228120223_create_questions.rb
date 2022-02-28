bcb206f76ff45b61a9beb61dedbe5a2def6930a3
Anna Krendeleva <krendelevaanna@mail.ru>
2022-02-28 16:55:45 +0300

    adding models and migrations

---
+++ db/migrate/20220228120223_create_questions.rb
@@ -0,0 +1,11 @@
+class CreateQuestions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :questions do |t|
+      t.string :body, null: false
+
+      t.timestamps
+
+      t.references :test, null: false, foreign_key: true
+    end
+  end
+end
