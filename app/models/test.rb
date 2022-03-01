c1188c0b3709464afafbbb1c48314219904135d0
Anna Krendeleva <krendelevaanna@mail.ru>
2022-03-01 14:23:14 +0300

    создан метод класса в модели Test

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,2 +1,6 @@
 class Test < ApplicationRecord
+    def self.by_category_title(category_title)
+        sql = 'INNER JOIN categories ON tests.category_id = categories.id'
+        joins(sql).where(categories: { title: category_title }).order(title: :desc).pluck(:title)
+    end
 end
