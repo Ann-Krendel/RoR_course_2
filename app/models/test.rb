8455757eca161bded0c3e1ae0f74662022dbe053
Anna Krendeleva <krendelevaanna@mail.ru>
2022-03-09 12:26:28 +0300

    Добавлены ассоциации к моделям

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
-
 class Test < ApplicationRecord
+  belongs_to :category
+  belongs_to :user
+  has_many :tests_users
+  has_many :users, through: :tests_user
+  has_many :questions
 
   def self.by_category_title(category_title)
     sql = 'INNER JOIN categories ON tests.category_id = categories.id'
