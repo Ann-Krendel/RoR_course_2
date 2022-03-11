219a7fb7084732a14ff1b35b044ec726aed2954a
Anna Krendeleva <krendelevaanna@mail.ru>
2022-03-11 09:40:14 +0300

    добавлены исправления

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 class User < ApplicationRecord
-  has_many :tests_users
-  has_many :tests, through: :tests_users
+  has_many :tests_users, dependent: :destroy
+  has_many :tests, through: :tests_users, dependent: :destroy
+  has_many :authored_tests, class_name: 'Test', foreign_key: :user_id
 
   def tests_by_level(test_level)
     sql = 'JOIN tests_users ON tests.id = tests_users.test_id'
     joins(sql)
       .where(tests_users: {user_id: id})
       .where(level: test_level)
-  end
-
-  def user_test(test)
-    test.tests_users.order(id: :desc).find_by(test: test)
   end
 end
